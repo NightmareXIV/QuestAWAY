42a74b4859df0cdb4f5344a64a7ccc18b19f7795
Eternita <lysithea.star@gmail.com>
2021-07-22 13:00:32 +0300

    Add project files.

---
+++ QuestAWAY/Bitmask.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestAWAY
+{
+    class Bitmask
+    {
+        public static bool IsBitSet(short b, byte pos)
+        {
+            return (b & (1 << pos)) != 0;
+        }
+
+        public static void SetBit(ref short b, byte pos)
+        {
+            b |= (short)(1 << pos);
+        }
+
+        public static void ResetBit(ref short b, byte pos)
+        {
+            b &= (short)~(1 << pos);
+        }
+        public static bool IsBitSet(uint b, byte pos)
+        {
+            return (b & (1 << pos)) != 0;
+        }
+
+        public static void SetBit(ref uint b, byte pos)
+        {
+            b |= (uint)(1 << pos);
+        }
+
+        public static void ResetBit(ref uint b, byte pos)
+        {
+            b &= (uint)~(1 << pos);
+        }
+    }
+}
---
+++ QuestAWAY/QuestAWAY.cs
@@ -0,0 +1,72 @@
+﻿using Dalamud.Game.Internal;
+using Dalamud.Plugin;
+using FFXIVClientStructs.FFXIV.Client.Graphics;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestAWAY
+{
+    unsafe class QuestAWAY : IDalamudPlugin
+    {
+        public string Name => "QuestAWAY!";
+        internal DalamudPluginInterface pi;
+        ByteColor transparent = new ByteColor()
+        {
+            A = 0, B = 0, G = 0, R = 0
+        };
+
+        public void Dispose()
+        {
+            pi.Dispose();
+            pi.Framework.OnUpdateEvent -= Tick;
+        }
+
+        public void Initialize(DalamudPluginInterface pluginInterface)
+        {
+            pi = pluginInterface;
+            pi.Framework.OnUpdateEvent += Tick;
+        }
+
+        [HandleProcessCorruptedStateExceptions]
+        public void Tick(object _)
+        {
+            try
+            {
+                var o = pi.Framework.Gui.GetUiObjectByName("AreaMap", 1);
+                if (o == IntPtr.Zero)
+                {
+                    return;
+                }
+                var masterWindow = (AtkUnitBase*)o;
+                if (!masterWindow->IsVisible) return;
+                var mapCNode = (AtkComponentNode*)masterWindow->UldManager.NodeList[3];
+                for (var i = 6; i < mapCNode->Component->UldManager.NodeListCount; i++)
+                {
+                    var mapIconNode = (AtkComponentNode*)mapCNode->Component->UldManager.NodeList[i];
+                    var imageNode = (AtkImageNode*)mapIconNode->Component->UldManager.NodeList[4];
+                    var textureInfo = imageNode->PartsList->Parts[imageNode->PartId].UldAsset;
+                    var texType = textureInfo->AtkTexture.TextureType;
+                    if (texType == TextureType.Resource)
+                    {
+                        var texFileNamePtr = textureInfo->AtkTexture.Resource->TexFileResourceHandle->ResourceHandle.FileName;
+                        var texString = Marshal.PtrToStringAnsi(new IntPtr(texFileNamePtr));
+                        if (texString == "ui/icon/071000/071021.tex")
+                        {
+                            mapIconNode->AtkResNode.Color = transparent;
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                pi.Framework.Gui.Chat.Print(e.Message);
+            }
+        }
+    }
+}
